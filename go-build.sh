10397f726ba8e2f1a89e4e99bc72b1503447312b
Evandro Goncalves <egoncalves@senhasegura.com>
2022-12-14 13:43:34 +0000

    Remove some old files from the repository

--- go-build.sh
+++
@@ -1,5 +0,0 @@
-# Build for Linux Alpine
-env GOOS=linux GARCH=amd64 go build -v -a -tags cgo -installsuffix cgo -ldflags "-linkmode external -extldflags -static" -o build/linux/dsm .
-
-# Build for Windows
-env GOOS=windows CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -v -o build/windows/dsm.exe .
